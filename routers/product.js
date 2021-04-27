d4eaab6fb2691a5b49d62ffb767a8e54136bef67
ofirlevy6658 <ofirlevy6658@gmail.com>
2021-04-27 12:50:41 +0300

    add validation

--- routers/product.js
+++ routers/product.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = new express.Router();
 
-// the client send in post body {"url":"exe.com"}
+// the client send in get body {"url":"exe.com"}
 
 router.get("/api/product", async (req, res) => {
 	try {
-		// Here I send the req to Sergy
+		const url = req.body.url;
+		if (!url) throw new Error("bad request url parameter is missing");
+		// Here I send the url to Sergy
 		// and he return me the data and also send it to Gilad to save the data in db
-		// const productData = await function()
+		// const productData = await function(url)
 		//then I send the data back to client side
-		res.status(201).send(req.body.url);
+		res.status(201).send(url);
 	} catch (e) {
 		res.status(400).send({ error: e.message });
 	}
