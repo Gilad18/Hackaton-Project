21d2100d0a0f8c7e51238ff7ca82170efd138dfa
wassimahaddad <wass75@gmail.com>
2021-04-27 17:40:13 +0300

    17:40

--- client/src/Pages/Search.jsx
+++ client/src/Pages/Search.jsx
@@ -1,25 +1,43 @@
 import React, { useState } from "react";
+
 import axios from "axios";
-const URL = "get for aws people";
+import "./Search.css";
+const URL = "http://localhost:5000/api/product";
 
 const Search = () => {
   const [data, setData] = useState("");
   const [term, setTerm] = useState("");
-  const getData = async () => {
-    const response = await axios.get(URL);
-    console.log(response);
-    setData(response);
-  };
+  const [direction, setDirection] = useState("rtl");
+
   const sendData = async () => {
-    const response = await axios.post(URL, term);
-    console.log(response);
+    const response = await axios({
+      method: "post",
+      url: URL,
+      data: {
+        url: term,
+      },
+    });
+    console.log(response.data);
+    setData(response.data);
   };
+  console.log(direction);
   return (
     <div>
-      <input onChange={(e) => setTerm(e.target.value)} value={term} />
-      <button onClick={sendData}>send</button>
-      <button onClick={getData}>receive</button>
-      <div>{data}</div>
+      <div className="search-bar">
+        <label>Paste URL here:</label>
+        <input onChange={(e) => setTerm(e.target.value)} value={term} />
+        <select onChange={(e) => setDirection(e.target.value)}>
+          <option value="rtl">العربية</option>
+          <option value="ltr">русский</option>
+          <option value="ltr">Français</option>
+        </select>
+        <button onClick={sendData}>Translate</button>
+      </div>
+      <div className={direction}>
+        <div>{data.title}</div>
+        <div>{data.price}</div>
+        <div>{data.url}</div>
+      </div>
     </div>
   );
 };
