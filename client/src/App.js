ad0e0a04121aee48f0383bdcd2d3ea07f701eed6
miraleJ <miralek@gmail.com>
2021-04-27 16:51:31 +0300

    favorites

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Favorites from './Pages/Favorites.page'
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
